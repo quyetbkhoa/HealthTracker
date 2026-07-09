70cedd00c84714bdb925138df288800f0fdc0166
nguyenducquyet <abcxyzasdf@gmail.com>
2026-07-09 19:27:14 +0700

    feat(app): add base Compose activity and theme

---
+++ app/src/main/java/com/quyetbkhoa/healthtracker/core/designsystem/Color.kt
@@ -0,0 +1,19 @@
+package com.quyetbkhoa.healthtracker.core.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+//app
+val PrimaryGreen = Color(0xFF4CAF50)
+val LightGreenBg = Color(0xFFE8F5E9)
+val TextPrimary = Color(0xFF1E1E1E)
+val TextSecondary = Color(0xFF757575)
+
